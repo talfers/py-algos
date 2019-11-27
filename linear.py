2dc08c28129907057249f9d13dca5302d04a54ac
tayloralfers@gmail.com <Taylor>
2019-11-26 21:04:29 -0700

    optimized linear

--- linear.py
+++ linear.py
@@ -1,11 +1,9 @@
 # linear search function
 def linear_search(arr, val):
-    i = 0
-    while i < len(arr):
+    for i in range(len(arr)):
         if arr[i] == val:
             print(i)
             return i
-        i+=1
     print(-1)
     return -1
 
@@ -13,4 +11,4 @@ def linear_search(arr, val):
 from test_data import test_arr
 
 # execute function
-linear_search(test_arr, 38)
+linear_search(test_arr, 66)
